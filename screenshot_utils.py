939f7873baaa64a1e317e0e3ddd32636d95c8f53
jonesywolf <4johnryanwolf@gmail.com>
2022-03-12 21:21:28 -0500

    Updated gitignore, cleaned up screenshot_utils

--- screenshot_utils.py
+++ screenshot_utils.py
@@ -2,6 +2,12 @@ from PIL import Image
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from pathlib import Path
+
+def fix_img_path(img_path):
+    img_path = img_path if img_path.endswith(".png") else img_path + ".png"
+    img_path = img_path if img_path.startswith("screenshots/") else "screenshots/" + img_path
+    return img_path
 
 def screenshot_page(browser, page_img_path):
     badge_elem_header = WebDriverWait(browser, 5).until(
@@ -11,8 +17,16 @@ def screenshot_page(browser, page_img_path):
     badge_elem = badge_elem_child.find_element(By.XPATH, "..")
     
     print("Taking a screenshot of the page...")
-    page_img_path = page_img_path if page_img_path.endswith(".png") else page_img_path + ".png"
-    page_img_path = page_img_path if page_img_path.startswith("screenshots/") else "screenshots/" + page_img_path
+    
+    try:
+        Path("screenshots/").mkdir()
+    except FileExistsError:
+        # Screenshots folder already exists
+        pass
+    else:
+        print("Created screenshots folder.")
+    
+    page_img_path = fix_img_path(page_img_path)
     browser.save_screenshot(page_img_path);
     print(f"Saved to {page_img_path}.")
     return badge_elem
@@ -26,13 +40,11 @@ def screenshot_badge(badge_img_path, page_img_path, badge_elem):
     width = x + elem_size["width"];
     height = y + elem_size["height"];
     print("Taking a screenshot of the badge...")
-    page_img_path = page_img_path if page_img_path.endswith(".png") else page_img_path + ".png"
-    page_img_path = page_img_path if page_img_path.startswith("screenshots/") else "screenshots/" + page_img_path
+    page_img_path = fix_img_path(page_img_path)
     im = Image.open(page_img_path)
     im = im.crop((int(x), int(y), int(width), int(height)))
     
-    badge_img_path = badge_img_path if badge_img_path.endswith(".png") else badge_img_path + ".png"
-    badge_img_path = badge_img_path if badge_img_path.startswith("screenshots/") else "screenshots/" + badge_img_path
+    badge_img_path = fix_img_path(badge_img_path)
     im.save(badge_img_path)
     print(f"Saved to {badge_img_path}.")
 
